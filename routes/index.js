22f4afcde75a4909f9c665ecc8ef6a25db62da6b
TuomoE <tuomo@ellila.fi>
2015-01-14 15:11:02 +0200

    lisäystä

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Login page' });
 });
 
+//router.get('/login', function(req,res) {
+  
+//  res.render('addressbook', { title: 'Addressbook' });
+//});
+router.login = function (req, res) {
+  
+  res.render('addressbook', { title: 'Addressbook' });
+}
 module.exports = router;
